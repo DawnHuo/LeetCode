d47402a3fcc2526178c01695a70c00e540dd5f87
unknown <472752575@qq.com>
2018-05-08 23:38:52 +0800

    Asteroid Collision

--- Asteroid_Collision.java
+++ Asteroid_Collision.java
@@ -8,6 +8,21 @@ Find out the state of the asteroids after all collisions. If two asteroids meet,
 
 **/
 
+//Clear Code
+class Solution {
+    public int[] asteroidCollision(int[] a) {
+        LinkedList<Integer> s = new LinkedList<>(); // use LinkedList to simulate stack so that we don't need to reverse at end.
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > 0 || s.isEmpty() || s.getLast() < 0)
+                s.add(a[i]);
+            else if (s.getLast() <= -a[i])
+                if (s.pollLast() < -a[i]) i--;
+        }
+        return s.stream().mapToInt(i->i).toArray();
+    }
+}
+
+//Previous Code
 class Solution {
 	public int[] asteroidCollision(int[] asteroids) {
 		int explodeCount = 0;
