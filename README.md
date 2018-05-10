f2edfed1939aac9b848923fa365bf3bc2069fb70
unknown <472752575@qq.com>
2018-05-11 00:16:10 +0800

    dp

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # LeetCode
+
+***
+
+### Problems
+
+1. Two Sum
+7. Reverse Integer
+70. Climbing Stairs
+518. Coin Change 2
+735. Asteroid Collision
--- Two_Sum.java
+++ Two_Sum.java
@@ -5,8 +5,28 @@
 *
 * You may assume that each input would have exactly one solution, and you may not use the same element twice.
 *
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
 **/
 
+//O(n)
+public int[] twoSum(int[] nums, int target) {
+	HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+	int[] result = new int[2];
+	for(int i = 0; i < nums.length; i++) {
+		if(map.containsKey(target - nums[i])) {
+			result[0] = map.get(target - nums[i]);
+			result[1] = i;
+			return result;
+		}
+		map.put(nums[i], i);
+	}
+	return result;
+}
+
+//my solution
 class Solution {
 	public int[] twoSum(int[] nums, int target) {
 		int[] s = new int[2];
